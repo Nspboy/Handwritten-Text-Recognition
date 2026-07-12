262f277340adba123aa354b921ebf957e88c8bb3
Nspboy <nagarajanagaraj.2003@gmail.com>
2026-07-12 20:29:16 +0530

    clean project done

--- Feature_Enhanced_HTR/hash_uploads.py
+++
@@ -1,8 +0,0 @@
-﻿import hashlib
-import os
-
-for f in os.listdir('uploads'):
-    if f.endswith(('.png', '.jpg')):
-        with open(os.path.join('uploads', f), 'rb') as file:
-            data = file.read()
-            print(f, hashlib.md5(data).hexdigest())
